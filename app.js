33b97b66e4e0ab17722f15d043665298befdba1b
mkjun14 <83442556+mkjun14@users.noreply.github.com>
2021-10-25 11:52:40 -0500

    implement hash using md5

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const { response } = require("express");
 
 const app = express();
@@ -26,9 +26,9 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = process.env.SECRET;
+// const secret = process.env.SECRET;
 
-userSchema.plugin(encrypt, { secret: secret, encyptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: secret, encyptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -47,7 +47,7 @@ app.get("/login", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser.save(function (err) {
@@ -61,7 +61,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username }, function (err, foundUser) {
     if (err) {
