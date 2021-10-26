0348fced90a724f335a8ad974fb3ebffa0e8c103
mkjun14 <83442556+mkjun14@users.noreply.github.com>
2021-10-25 21:01:32 -0500

    download bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
 const { response } = require("express");
 
 const app = express();
