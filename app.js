f71a541b391ff69194d8ed822d34784399292f15
mkjun14 <83442556+mkjun14@users.noreply.github.com>
2021-10-29 11:53:08 -0500

    download express-session passport passport-local-mongoose

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const bcrypt = require("bcrypt");
-const { response } = require("express");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
+const { response } = require("express");
 const app = express();
 
 app.use(express.static("public"));
@@ -45,42 +46,9 @@ app.get("/login", function (req, res) {
   res.render("login");
 });
 
-app.post("/register", function (req, res) {
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash,
-    });
+app.post("/register", function (req, res) {});
 
-    newUser.save(function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-  });
-});
-
-app.post("/login", function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({ email: username }, function (err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function (err, result) {
-          // result == true
-          if (result === true) {
-            res.render("secrets");
-          }
-        });
-      }
-    }
-  });
-});
+app.post("/login", function (req, res) {});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
